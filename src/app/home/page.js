8baa9317b434c0604bc693eacdc570992623df9f
giandab <giandabdouk@gmail.com>
2025-10-24 21:08:58 +0100

    added deposit/withdraw pages

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -12,14 +12,25 @@ export default async function Home({searchParams}){
     let message = await response.text()
     console.log(message)
     message = JSON.parse(message)
+
+    let positions = []
+    for(const row of message["message"]){
+        positions.push([row[1],row[2]])
+    }
+
     
     const username = await searchParams.username
     const password = await searchParams.password
     return (<>
         <h2>Welcome home {body.username}</h2>
-        <h3>{message["message"]}</h3>
+        <h3>{positions}</h3>
         <Form action="/buySell">
             <input type="submit" value="Buy or Sell Product"></input>
+            <input type='hidden' value={username} name='username'></input>
+            <input type='hidden' value={password} name='password'></input>
+        </Form>
+        <Form action="/depositWithdraw">
+            <input type="submit" value="Deposit or Withdraw cash"></input>
             <input type='hidden' value={username} name='username'></input>
             <input type='hidden' value={password} name='password'></input>
         </Form>
