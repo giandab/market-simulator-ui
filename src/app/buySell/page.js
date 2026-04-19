368f56b2369216bd463fade0d819419a8b407633
giandab <giandabdouk@gmail.com>
2026-04-19 11:53:24 +0100

    transaction pages

--- src/app/buySell/page.js
+++ src/app/buySell/page.js
@@ -1,8 +1,54 @@
 import Form from 'next/form'
 import { redirect } from 'next/navigation'
+import Navbar from '@/components/shadcn-studio/blocks/navbar-component-01/navbar-component-01'
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default async function BuyOrSell({searchParams}){
     const input = await searchParams
+    const username = await searchParams.username
+    const password = await searchParams.password
+
+        const navigationData = [
+  {
+    title: 'Home',
+    href: '/home?username='+username+"&password="+password
+  },
+  {
+    title: 'Buy/Sell',
+    href: '/buySell?username='+username+"&password="+password
+  },
+  {
+    title: 'Deposit/Withdraw',
+    href: '/depositWithdraw?username='+username+"&password="+password
+  },
+  {
+    title: 'Transactions',
+    href: '#'
+  }
+]
     async function buy(formData){
         'use server'
 
@@ -30,17 +76,58 @@ export default async function BuyOrSell({searchParams}){
         return (<><h3>{input.res}</h3></>)
     }
     
-    return (<>
-        <h2>Buy or Sell asset</h2>
-        <Form action={buy}>
-            <input type="text" name="name" placeholder="Product"></input>
-            <input type="number" name="amount" placeholder="Amount"></input>
-            <select name='buy/sell' id='buy/sell'>
-            <option value="Buy">Buy</option>
-            <option value="Sell">Sell</option>
-            </select>
-            <input type="submit" value="Submit"></input>
-        </Form>
-        </>
+    return (
+        <div flex flex-col>
+        <Navbar navigationData={navigationData}></Navbar>
+    <br/><br/>
+    <div className='flex flex-1 flex-col items-center'>
+     <div className="flex flex-1 flex-col gap-6" style={{width:"20%"}}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Buy or sell Product</CardTitle>
+              <CardDescription>
+                Type a ticker name and select whether you want to buy or sell
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form action={buy}>
+                <FieldGroup>
+                  <Field>
+                    <FieldLabel htmlFor="email">Ticker</FieldLabel>
+                    <Input name="name" id="name" required />
+                  </Field>
+                  <Field>
+                    <div className="flex items-center">
+                      <FieldLabel htmlFor="password">Amount</FieldLabel>
+                    </div>
+                    <Input name="amount" id="amount" type="number" required />
+                  </Field>
+                  <Field>
+                    <Select
+        name="buy/sell"
+      >
+        <SelectTrigger
+          id="form-rhf-select-language"
+          className="min-w-[120px]"
+        >
+          <SelectValue placeholder="Select" />
+        </SelectTrigger>
+        <SelectContent position="item-aligned">
+          <SelectItem value="Buy">Buy</SelectItem>
+          <SelectItem value="Sell">Sell</SelectItem>
+        </SelectContent>
+      </Select>
+                  </Field>
+                  <Field>
+                    <Button type="submit">Execute</Button>
+                    <br></br>
+                  </Field>
+                </FieldGroup>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+        </div>
+        </div>
     )
 }
--- src/app/depositWithdraw/page.js
+++ src/app/depositWithdraw/page.js
@@ -1,8 +1,54 @@
 import Form from 'next/form'
 import { redirect } from 'next/navigation'
+import Navbar from '@/components/shadcn-studio/blocks/navbar-component-01/navbar-component-01'
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default async function DepositWithdraw({searchParams}){
     const input = await searchParams
+        const username = await searchParams.username
+    const password = await searchParams.password
+
+        const navigationData = [
+  {
+    title: 'Home',
+    href: '/home?username='+username+"&password="+password
+  },
+  {
+    title: 'Buy/Sell',
+    href: '/buySell?username='+username+"&password="+password
+  },
+  {
+    title: 'Deposit/Withdraw',
+    href: '/depositWithdraw?username='+username+"&password="+password
+  },
+  {
+    title: 'Transactions',
+    href: '#'
+  }
+]
     
     async function transact(formData){
         'use server'
@@ -27,16 +73,65 @@ export default async function DepositWithdraw({searchParams}){
         return (<><h3>{input.res}</h3></>)
     }
 
-    return (<>
-        <h2>Deposit or Withdraw cash</h2>
-        <Form action={transact}>
-            <input type="number" name="amount"></input>
-            <select name='deposit/withdraw' id='deposit/withdraw'>
-            <option value="Deposit">Deposit</option>
-            <option value="Withdraw">Withdraw</option>
-            </select>
-            <input type="submit" value="Submit"></input>
-        </Form>
-        </>
+    return (
+    // <>
+    //     <h2>Deposit or Withdraw cash</h2>
+    //     <Form action={transact}>
+    //         <input type="number" name="amount"></input>
+    //         <select name='deposit/withdraw' id='deposit/withdraw'>
+    //         <option value="Deposit">Deposit</option>
+    //         <option value="Withdraw">Withdraw</option>
+    //         </select>
+    //         <input type="submit" value="Submit"></input>
+    //     </Form>
+    //     </>
+            <div flex flex-col>
+        <Navbar navigationData={navigationData}></Navbar>
+    <br/><br/>
+    <div className='flex flex-1 flex-col items-center'>
+     <div className="flex flex-1 flex-col gap-6" style={{width:"20%"}}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Deposit or Withdraw Cash</CardTitle>
+              <CardDescription>
+                Enter an amount to deposit or withdraw
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form action={transact}>
+                <FieldGroup>
+                  <Field>
+                    <div className="flex items-center">
+                      <FieldLabel htmlFor="password">Amount</FieldLabel>
+                    </div>
+                    <Input name="amount" id="amount" type="number" required />
+                  </Field>
+                  <Field>
+                    <Select
+        name="deposit/withdraw"
+      >
+        <SelectTrigger
+          id="form-rhf-select-language"
+          className="min-w-[120px]"
+        >
+          <SelectValue placeholder="Select" />
+        </SelectTrigger>
+        <SelectContent position="item-aligned">
+          <SelectItem value="Deposit">Deposit</SelectItem>
+          <SelectItem value="Withdraw">Withdraw</SelectItem>
+        </SelectContent>
+      </Select>
+                  </Field>
+                  <Field>
+                    <Button type="submit">Execute</Button>
+                    <br></br>
+                  </Field>
+                </FieldGroup>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+        </div>
+        </div>
     )
 }
